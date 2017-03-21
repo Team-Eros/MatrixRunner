407f322dfd5b737687b13debdc120380dfe6ae34
zachdimitrov <zahari_d@abv.bg>
2017-03-21 11:34:27 +0200

    Add jump with space key. Added favicon. Separated jump method in keyboard control.

--- js/keyboard.js
+++ js/keyboard.js
@@ -3,8 +3,6 @@
 function control(body) {
     window.addEventListener('keydown', function(ev) {
         console.log(ev.which);
-
-        let floor = FIELD_HEIGHT - body.height - 26;
         switch (ev.which) {
             case 37:
                 if (body.coords.x <= 0) {
@@ -15,10 +13,10 @@ function control(body) {
                 body.speed.x = -body.impulse.x;
                 break;
             case 38:
-                if (body.coords.y < floor - body.height || body.speed.y > 0) {
-                    return;
-                }
-                body.speed.y = -body.impulse.y;
+                jump();
+                break;
+            case 32:
+                jump();
                 break;
             case 39:
                 body.speed.x = body.impulse.x;
@@ -32,6 +30,14 @@ function control(body) {
                 break;
         }
     });
+
+    function jump() {
+        let floor = FIELD_HEIGHT - body.height - 26;
+        if (body.coords.y < floor - body.height || body.speed.y > 0) {
+            return;
+        }
+        body.speed.y = -body.impulse.y;
+    }
 
     // window.addEventListener('keyup', function(ev) {
     //     if (ev.which !== 38 && body.y >= floor - 26) {
