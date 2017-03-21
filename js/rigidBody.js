aab35f7c8541e324bf1276948a6778fb43d70876
zachdimitrov <zahari_d@abv.bg>
2017-03-21 03:50:27 +0200

    create rigidBody.js file for main game physics

---
+++ js/rigidBody.js
@@ -0,0 +1,78 @@
+// creates object from provided options {position, size, speed, impulse, acceleration...}
+
+// has methods
+
+// move
+// collide
+
+class RigidBody {
+    constructor(coords, speed, width, height, impulse) {
+        this.coords = { x: coords.x, y: coords.y };
+        this.speed = speed || { x: 0, y: 0 };
+        this.width = width;
+        this.height = height;
+        this.impulse = impulse || { x: 1, y: 1 };
+    }
+
+    move() {
+        let lastPosition = { x: this.coords.x, y: this.coords.y };
+        this.coords.x += this.speed.x;
+        this.coords.y += this.speed.y;
+        return lastPosition;
+    }
+
+    decelerate(friction) {
+        if (this.speed.y === 0) {
+            if (this.coords.x < 0) {
+                this.coords.x = 0;
+            }
+            if (this.coords.x > FIELD_WIDTH - this.width) {
+                this.coords.x = FIELD_WIDTH - this.width;
+            }
+            if (this.speed.x > 0) {
+                this.speed.x -= friction;
+                if (this.speed.x < 0) {
+                    this.speed.x = 0;
+                }
+            }
+            if (this.speed.x < 0) {
+                this.speed.x += friction;
+                if (this.speed.x > 0) {
+                    this.speed.x = 0;
+                }
+            }
+        }
+        return this;
+    }
+
+    applyGravity(gravity) {
+        // needs refactoring
+        let floor = FIELD_HEIGHT - this.height - 26;
+        if (this.coords.y === floor) {
+            return this;
+        }
+
+        if (this.coords.y > floor) {
+            this.coords.y = floor;
+            this.speed.y = 0;
+            return this;
+        }
+
+        if (this.coords.y < floor) {
+            this.speed.y += gravity;
+        }
+        return this;
+    }
+
+    collidesWith(otherBody) {
+        // needs refactoring
+        var thisRadius = (this.width + this.height) / 4,
+            otherRadius = (otherBody.width + otherBody.height) / 4,
+            x1 = this.coords.x + this.width / 2,
+            y1 = this.coords.y + this.height / 2,
+            x2 = otherBody.coords.x + otherBody.width / 2,
+            y2 = otherBody.coords.y + otherBody.height / 2,
+            distance = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        return distance <= thisRadius + otherRadius;
+    }
+}
